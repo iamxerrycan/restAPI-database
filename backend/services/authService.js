ab6c27fb70a91395f2f4870333060e163668eeb4
iamxerrycan <rajshish333@gmail.com>
2025-06-26 14:54:52 +0530

    fix

--- backend/services/authService.js
+++ backend/services/authService.js
@@ -1,4 +1,4 @@
-const User = require('../models/User');
+const User = require('../models/userModel');
 const { generateToken } = require('../utils/jwtHelper');
 
 const registerService = async ({ name, email, password }) => {
